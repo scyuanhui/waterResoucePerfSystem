b0208181fc6a9074b5b1705ddce727b709fdeb4a
qjwvtd <176156562@qq.com>
2018-07-20 19:02:48 +0800

    first

---
+++ web/src/home/home.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Home extends Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+    render(){
+        return (
+            <div>
+                <h5>home</h5>
+                <img src="static/img/loginBg.jpg" />
+            </div>
+        );
+    }
+}
--- web/src/index.js
+++ web/src/index.js
@@ -2,10 +2,14 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 //login
 import LoginView from './login/login';
+//home
+import Home from './home/home';
 //css
 import './static/css/iconfont.css';
 import './static/css/public.css';
 import './static/css/login.css';
+
+
 
 ReactDOM.render(
     <LoginView />,
