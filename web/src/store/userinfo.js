9e713fee438fed7e5d3f4a41d2db80bcd5ed6709
qjwvtd <176156562@qq.com>
2018-07-25 18:44:22 +0800

    2018-07-25 添加状态管理库

--- web/src/store/userinfo.js
+++ web/src/store/userinfo.js
@@ -1,21 +1,22 @@
 import {observable,action} from 'mobx';
 
-const grades = ['province','city','county'];
-const units = ['四川省水利厅','成都市水利局','金牛区水务处'];
-const sysLvl = ['省级系统','市级系统','县级系统'];
-const ran = (Math.random() * 2).toFixed(0);
-const ranGrade = grades[ran];
-const ranUnit = units[ran];
-const ranSysLvl = sysLvl[ran];
-
 class User{
+    constructor(){
+        this.grades = ['province','city','county'];
+        this.units = ['四川省水利厅','成都市水利局','金牛区水务处'];
+        this.sysLvl = ['省级系统','市级系统','县级系统'];
+        this.ran = (Math.random() * 2).toFixed(0);
+        this.ranGrade = this.grades[this.ran];
+        this.ranUnit = this.units[this.ran];
+        this.ranSysLvl = this.sysLvl[this.ran];
+    }
     @observable data = {
         //模拟用户，(province)省级，(city)市级，(county)区县级
         username: 'admin',
         token: '5w4f8gfnbv2d812',
-        userGrade:ranGrade,
-        userUnit:ranUnit,
-        sysLvl:ranSysLvl
+        userGrade:this.ranGrade,
+        userUnit:this.ranUnit,
+        sysLvl:this.ranSysLvl
     };
     @action.bound login(username){
         this.data.username = username;
