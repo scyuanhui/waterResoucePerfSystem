8ccc30447b96b3ce5a2bf679c9add3000bde73ce
qjwvtd <176156562@qq.com>
2018-07-25 17:43:39 +0800

    2018-07-25 添加状态管理库

--- web/src/public/common.js
+++ web/src/public/common.js
@@ -69,3 +69,28 @@ export class Axios{
         });
     }
 }
+export function formatDate(dateStr,reg) {
+    if(!dateStr){
+        return;
+    }
+    var _data = new Date(dateStr);
+    var year = _data.getFullYear();
+    var month = this.setLen(_data.getMonth() + 1);
+    var date = setLen(_data.getDate());
+    var hour = setLen(_data.getHours());
+    var minute = setLen(_data.getMinutes());
+    var second = setLen(_data.getSeconds());
+    var _reg = reg ? reg : '-';
+    return {
+        'YEAR':year,
+        'MONTH':month,
+        'DATE':date,
+        'YMD':year + _reg + month + _reg + date,
+        'HMS':hour + ":" + minute + ":" + second,
+        'YMD_HMS':year + _reg + month + _reg + date + " " + hour + ":" + minute + ":" + second
+    };
+    function setLen(str){
+        str = str.toString().length < 2 ? 0 + str.toString() : str;
+        return str;
+    }
+}
