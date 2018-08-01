b6e196e063d80d8ae90d5f5d4b9c56cdead09047
qjwvtd <176156562@qq.com>
2018-08-01 16:20:22 +0800

    2018-07-31

--- web/src/public/common.js
+++ web/src/public/common.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+export const root = '';
 //session
 export class Session{
     setItem(key,value){
