17eee58d8eb5547891fad373c119567ec335c926
qjwvtd <176156562@qq.com>
2018-08-06 11:16:31 +0800

    2018-07-31

--- web/src/store/interface.js
+++ web/src/store/interface.js
@@ -2,7 +2,7 @@ const root = 'http://192.168.10.13:8080';
 const api = {
     login: root + '/shuilizijin/login',//用户登录
     logout: root + '/shuilizijin/logout',//用户登出
-    getOneList: root + '/pfmIndex/getByLevel',//获取一级（5个步骤）
+    getOneList: root + '/pfmIndex/getByLevel',//获取一级指标（5个步骤）
     getCountyPerList:root + '/shuilizijin/record/query',//获取县级绩效列表
     getCountyPerDeclare:root + '/shuilizijin/pfmIndex/query',//绩效指标申报-申报
     addPerDeclare:root + '/shuilizijin/examine/add',//绩效指标申报-提交
